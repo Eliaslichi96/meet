bf457f7e4728593d5cb49934b6da0780b89e9308
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2024-04-14 22:02:25 -0400

    4.9 update

--- src/api.js
+++ src/api.js
@@ -26,6 +26,10 @@ export const getEvents = async () => {
     if (window.location.href.startsWith("http://localhost")) {
         return mockData;
     }
+    if (!navigator.onLine) {
+        const events = localStorage.getItem("lastEvents");
+        return events ? JSON.parse(events) : [];
+    }
 
     const token = await getAccessToken();
 
@@ -35,7 +39,6 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
-            NProgress.done();
             localStorage.setItem("lastEvents", JSON.stringify(result.events));
             return result.events;
         } else return null;
